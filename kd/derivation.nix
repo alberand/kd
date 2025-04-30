8008dae37c0b59541f28c272d1ed3f3f491cab1d
Andrey Albershteyn <aalbersh@kernel.org>
2025-04-30 10:34:04 +0200

    kd: add nurl as dependncy

--- kd/derivation.nix
+++ kd/derivation.nix
@@ -1,15 +1,11 @@
-{makeRustPlatform, rust-bin}:
-let
-  rustPlatform = makeRustPlatform {
-    cargo = rust-bin.nightly.latest.minimal;
-    rustc = rust-bin.nightly.latest.minimal;
-  };
-in
-
+{rustPlatform, nurl}:
 rustPlatform.buildRustPackage {
   pname = "kd";
   version = "0.0.1";
 
   src = ./.;
   cargoHash = "sha256-XdA1j86bdHGBu4q6WuZePWSobXBLhXQwnKz/akUVR6s=";
+  buildInputs = [
+    nurl
+  ];
 }
