447ca1190dc52d26a5c04211ea0cc8fefa6fa063
Andrey Albershteyn <aalbersh@kernel.org>
2025-07-09 22:16:18 +0200

    Add flake tests
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     nixos-generators,
     rust-overlay,
   }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
@@ -80,6 +80,62 @@
 
       templates.default = {
         path = ./templates/vm;
+      };
+
+      checks = {
+        "${system}".default = pkgs.testers.runNixOSTest {
+          name = "basic-test";
+          nodes = {
+            machine = {pkgs, ...}: {
+              imports = [
+                (import ./xfstests/module.nix {})
+                (import ./xfsprogs/module.nix {})
+                ./dummy.nix
+                ./system.nix
+                (pkgs.callPackage (import ./input.nix) {inherit nixpkgs;})
+                ({config, ...}: let
+                  buildKernelHeaders = pkgs.makeLinuxHeaders;
+                  sources = pkgs.lib.importJSON ./sources/kernel.json;
+                in {
+                  networking.hostName = "kd-test";
+                  kernel = {
+                    src = pkgs.fetchgit sources;
+                    version = sources.rev;
+                  };
+                  vm.workdir = "/tmp/kd-test/";
+                  vm.disks = [12000 12000 2000 2000];
+
+                  programs.xfsprogs = {
+                    enable = true;
+                    kernelHeaders = buildKernelHeaders {
+                      inherit (config.kernel) src version;
+                    };
+                  };
+                  programs.dummy = {
+                    enable = true;
+                  };
+                  programs.xfstests = {
+                    enable = true;
+                    test-dev = pkgs.lib.mkDefault "/dev/vdb";
+                    scratch-dev = pkgs.lib.mkDefault "/dev/vdc";
+                    arguments = "-R xunit -s xfs_4k generic/110";
+                    kernelHeaders = buildKernelHeaders {
+                      inherit (config.kernel) src version;
+                    };
+                  };
+                })
+              ];
+              environment.systemPackages = [pkgs.curl];
+            };
+          };
+
+          testScript = ''
+            machine.start()
+            machine.wait_for_unit("xfstests")
+            status = machine.succeed("systemctl show --property=ExecMainStatus xfstests.service")
+            assert status == "ExecMainStatus=1"
+          '';
+        };
       };
     });
 }
