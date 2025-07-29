11b9728ad674dc2800c954be545555b9db28a730
Andrey Albershteyn <aalbersh@kernel.org>
2025-07-29 13:40:13 +0200

    kd: update cargoHash
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- kd/derivation.nix
+++ kd/derivation.nix
@@ -9,7 +9,7 @@ rustPlatform.buildRustPackage {
   version = "0.0.1";
 
   src = ./.;
-  cargoHash = "sha256-2ArjfJPc0Xi7B2bcW63YeUmdQaHej71sZS4OqS8j6wY=";
+  cargoHash = "sha256-81umLjXpcGrjfD/wwCHVcaiVF1o/ju777DtfPN6LGPc=";
   nativeBuildInputs = [
     pkg-config
     nurl
