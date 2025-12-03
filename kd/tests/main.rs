a744788d26d78e562a6780ba1776944714837349
Andrey Albershteyn <aalbersh@kernel.org>
2025-12-03 19:43:00 +0100

    kd: add tests skeleton for kd commands

---
+++ kd/tests/main.rs
@@ -0,0 +1,24 @@
+#[test]
+fn kd_init() {
+    assert_eq!(0, 0);
+}
+
+#[test]
+fn kd_build() {
+    assert_eq!(0, 0);
+}
+
+#[test]
+fn kd_run() {
+    assert_eq!(0, 0);
+}
+
+#[test]
+fn kd_update() {
+    assert_eq!(0, 0);
+}
+
+#[test]
+fn kd_config() {
+    assert_eq!(0, 0);
+}
