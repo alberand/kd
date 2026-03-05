d4f0ca41d5f55a3539642bd739da5a0462ed7c11
Andrey Albershteyn <aalbersh@kernel.org>
2026-03-05 22:02:45 +0100

    template: drop flake-utils and don't follow nixpkgs
    Don't follow to use prebuilt artifacts

--- templates/vm/flake.nix
+++ templates/vm/flake.nix
@@ -13,33 +13,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
     kd.url = "github:alberand/kd";
-    kd.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     kd,
-  }:
-    flake-utils.lib.eachDefaultSystem (_: let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [kd.overlays.default];
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [kd.overlays.default];
+    };
+    packages = let
+      uset = import ./uconfig.nix;
+    in
+      kd.lib.mkEnv {
+        inherit nixpkgs;
+        uconfig = uset.uconfig {inherit pkgs kd;};
       };
-      packages = let
-        uset = import ./uconfig.nix;
-      in
-        kd.lib.mkEnv {
-          inherit nixpkgs;
-          uconfig = uset.uconfig {inherit pkgs kd;};
-        };
-    in {
-      inherit packages;
-      devShells = {
-        default = packages.shell;
-      };
-    });
+  in {
+    inherit packages;
+    devShells.${system} = {
+      default = packages.shell;
+    };
+  };
 }
