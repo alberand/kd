79c2036e05bd5cd4a5334e1e70a1dd77d2113972
Andrey Albershteyn <aalbersh@kernel.org>
2026-03-27 11:32:39 +0100

    kd: update deploy script to support new systemd-repart images

--- deploy.sh
+++ deploy.sh
@@ -15,13 +15,13 @@ PREFIX="aalbersh"
 SYSURI="qemu+ssh://$TEST_HOST/system"
 NODE="$PREFIX-$2"
 
-echo "Removing /tmp/$NODE from $TEST_HOST"
-ssh $TEST_HOST "sudo rm -f -- /tmp/$NODE"
-
 echo "Destroy running node if exists"
 if virsh --connect $SYSURI list | grep -q "$NODE"; then
 	virsh --connect $SYSURI destroy $NODE
 fi
+
+echo "Removing /tmp/$NODE from $TEST_HOST"
+ssh $TEST_HOST "sudo rm -f -- /tmp/$NODE"
 
 echo "Uploading '$IMAGE' to '$TEST_HOST:/tmp/$NODE'"
 rsync -avz -P \
@@ -31,7 +31,12 @@ if [ $? -ne 0 ]; then
 	exit 1;
 fi;
 
-ssh $TEST_HOST "sudo chmod u+w /tmp/$NODE"
+ssh $TEST_HOST << ENDSSH
+DISK_IMAGE="/tmp/$NODE"
+chmod +w "\$DISK_IMAGE"
+qemu-img resize -f raw "\$DISK_IMAGE" "+30G"
+ENDSSH
+
 echo "Bringing up the node"
 virt-install --connect $SYSURI \
 	--name "$NODE" \
@@ -39,14 +44,9 @@ virt-install --connect $SYSURI \
 	--osinfo "nixos-unstable" \
 	--memory=8000 \
 	--vcpu 4 \
-	--disk path="/tmp/$NODE",format=qcow2 \
-	--disk size=20,target.bus=sata,format=raw \
-	--disk size=20,target.bus=sata,format=raw \
-	--disk size=1,target.bus=sata,format=raw \
-	--disk size=1,target.bus=sata,format=raw \
-	--disk size=1,target.bus=sata,format=raw \
-	--disk size=1,target.bus=sata,format=raw \
+	--disk path="/tmp/$NODE",target.bus=sata,driver.type=raw \
 	--network network=anet \
+	--boot loader=/usr/share/OVMF/OVMF_CODE.fd,loader.readonly=yes,loader.type=pflash,nvram.template=/usr/share/OVMF/OVMF_VARS.fd,loader_secure=no \
 	--import \
 	--serial pty \
 	--graphics none \
