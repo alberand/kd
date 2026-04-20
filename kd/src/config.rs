e7aaaa3362fce605d42e4b51ddbcec41f62a363b
Andrey Albershteyn <aalbersh@kernel.org>
2026-04-20 12:59:07 +0200

    kd: migrate errors to anyhow

--- kd/src/config.rs
+++ kd/src/config.rs
@@ -1,11 +1,9 @@
+use anyhow::{bail, Context, Result};
 use serde::{Deserialize, Serialize};
 use std::fs;
-use std::io::Write;
 use std::path::{absolute, Path, PathBuf};
 use toml;
 use toml::Table;
-
-use super::utils::{KdError, KdErrorKind};
 
 #[derive(Serialize, Deserialize, Clone, Default, Debug)]
 pub struct KernelConfigOption {
@@ -184,7 +182,6 @@ pub struct DevConfig {
     pub args: Option<Vec<String>>,
 }
 
-
 #[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Config {
     pub packages: Option<Vec<String>>,
@@ -199,33 +196,14 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn load<T: AsRef<Path>>(path: T) -> Result<Self, KdError> {
-        if !path.as_ref().exists() {
-            return Err(KdError::new(
-                KdErrorKind::RuntimeError,
-                "config file not found".to_string(),
-            ));
-        }
-
-        let data = fs::read_to_string(path)
-            .map_err(|e| KdError::new(KdErrorKind::IOError(e), "can not read".to_string()))?;
-        let config: Config = toml::from_str(&data)
-            .map_err(|e| KdError::new(KdErrorKind::TOMLError(e), "invalid TOML".to_string()))?;
+    pub fn load<T: AsRef<Path>>(path: T) -> Result<Self> {
+        let data = fs::read_to_string(path).context("Failed to read config")?;
+        let config: Config = toml::from_str(&data).context("Invalid TOML")?;
 
         Ok(config)
     }
 
-    pub fn _save<T: AsRef<Path>>(&self, path: T) -> Result<(), KdError> {
-        let mut buffer = std::fs::File::create(path)
-            .map_err(|e| KdError::new(KdErrorKind::IOError(e), "failed to create".to_string()))?;
-        buffer
-            .write_all(toml::to_string(self).unwrap().as_bytes())
-            .map_err(|e| KdError::new(KdErrorKind::IOError(e), "failed to write".to_string()))?;
-
-        Ok(())
-    }
-
-    pub fn validate(&self) -> Result<(), KdError> {
+    pub fn validate(&self) -> Result<()> {
         if let Some(subconfig) = &self.kernel {
             let kernel = subconfig.version.is_some()
                 || subconfig.rev.is_some()
@@ -239,22 +217,13 @@ impl Config {
         if let Some(subconfig) = &self.xfsprogs {
             if let Some(subconfig) = &subconfig.kernel_headers {
                 if subconfig.repo.is_none() {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        "You are missing 'repo' parameter for kernel headers".to_owned(),
-                    ));
+                    bail!("You are missing 'repo' parameter for kernel headers");
                 }
                 if subconfig.rev.is_none() {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        "You are missing 'rev' parameter for kernel headers".to_owned(),
-                    ));
+                    bail!("You are missing 'rev' parameter for kernel headers");
                 }
                 if subconfig.version.is_none() {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        "You are missing 'version' parameter for kernel headers".to_owned(),
-                    ));
+                    bail!("You are missing 'version' parameter for kernel headers");
                 }
             }
         }
@@ -262,75 +231,43 @@ impl Config {
         if let Some(subconfig) = &self.xfstests {
             if let Some(subconfig) = &subconfig.kernel_headers {
                 if subconfig.repo.is_none() {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        "You are missing 'repo' parameter for kernel headers".to_owned(),
-                    ));
+                    bail!("You are missing 'repo' parameter for kernel headers");
                 }
                 if subconfig.rev.is_none() {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        "You are missing 'rev' parameter for kernel headers".to_owned(),
-                    ));
+                    bail!("You are missing 'rev' parameter for kernel headers");
                 }
                 if subconfig.version.is_none() {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        "You are missing 'version' parameter for kernel headers".to_owned(),
-                    ));
+                    bail!("You are missing 'version' parameter for kernel headers");
                 }
             }
 
             if let Some(hooks) = &subconfig.hooks {
                 let path = PathBuf::from(hooks);
                 if !path.exists() {
-                    let cwd =
-                        std::env::current_dir().expect("Failed to retrieve current working dir");
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        format!("Failed to find '{:?}' dir (cwd is {:?})", path, cwd),
-                    ));
+                    let cwd = std::env::current_dir()
+                        .context("Failed to retrieve current working dir")?;
+                    bail!("Failed to find '{:?}' dir (cwd is {:?})", path, cwd);
                 }
             }
         }
 
         if let Some(subconfig) = &self.kernel {
             if subconfig.repo.is_some() && subconfig.rev.is_none() && subconfig.version.is_none() {
-                return Err(KdError::new(
-                    KdErrorKind::ConfigError,
-                    "While using 'repo' rev/version need to be set".to_owned(),
-                ));
+                bail!("While using 'repo' rev/version need to be set");
             }
 
             if subconfig.rev.is_some() && subconfig.version.is_none() {
-                return Err(KdError::new(
-                    KdErrorKind::ConfigError,
-                    "Revision can not be used without 'version'".to_owned(),
-                ));
+                bail!("Revision can not be used without 'version'");
             }
 
             if let Some(kernel) = &subconfig.prebuild {
-                let curdir = std::env::current_dir().map_err(|e| {
-                    KdError::new(
-                        KdErrorKind::IOError(e),
-                        "No able to get current working directory".to_string(),
-                    )
-                })?;
+                let curdir =
+                    std::env::current_dir().context("No able to get current working directory")?;
 
-                let path = absolute(curdir.join(kernel))
-                    .map_err(|e| {
-                        KdError::new(
-                            KdErrorKind::ConfigError,
-                            format!("Failed to parse kernel path: {}", e.to_string()),
-                        )
-                    })
-                    .unwrap();
+                let path = absolute(curdir.join(kernel)).context("Failed to parse kernel path")?;
 
                 if !(path.exists()) {
-                    return Err(KdError::new(
-                        KdErrorKind::ConfigError,
-                        format!("Kernel doesn't exists: {}", kernel),
-                    ));
+                    bail!("Kernel doesn't exists: {}", kernel);
                 }
             }
         }
--- kd/src/main.rs
+++ kd/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{bail, Context, Result};
 use clap::Parser;
 use std::collections::HashMap;
 use std::fs::File;
@@ -47,13 +48,8 @@ impl Default for State {
 }
 
 impl State {
-    fn new(config_path: Option<PathBuf>) -> Result<Self, KdError> {
-        let curdir = std::env::current_dir().map_err(|e| {
-            KdError::new(
-                KdErrorKind::IOError(e),
-                "No able to get current working directory".to_string(),
-            )
-        })?;
+    fn new(config_path: Option<PathBuf>) -> Result<Self> {
+        let curdir = std::env::current_dir().context("Unable to read current directory")?;
         let config_path = if let Some(config_path) = config_path {
             config_path
         } else {
@@ -331,38 +327,20 @@ fn generate_uconfig(state: &mut State) -> Result<String, KdError> {
     ))
 }
 
-fn cmd_init(_: &State) -> Result<(), KdError> {
-    let curdir = std::env::current_dir().map_err(|e| {
-        KdError::new(
-            KdErrorKind::IOError(e),
-            "No able to get current working directory".to_string(),
-        )
-    })?;
+fn cmd_init(_: &State) -> Result<()> {
+    let curdir = std::env::current_dir().context("No able to get current working directory")?;
     let config_path = curdir.clone().join(".kd.toml");
     match &mut File::create(&config_path) {
-        Ok(target) => {
-            writeln!(target, include_str!("config.tmpl")).map_err(|e| {
-                KdError::new(
-                    KdErrorKind::IOError(e),
-                    "Failed to write env name to .kd.toml: {e}".to_owned(),
-                )
-            })?;
-        }
+        Ok(target) => writeln!(target, include_str!("config.tmpl"))
+            .context("Failed to write env name to .kd.toml")?,
         Err(error) => {
-            return Err(KdError::new(
-                KdErrorKind::RuntimeError,
-                format!("Unable to create {}: {}", config_path.display(), error),
-            ));
+            bail!("Unable to create {}: {}", config_path.display(), error);
         }
     };
 
     let flake_dir = curdir.clone().join(".kd/flake");
-    if let Err(error) = std::fs::create_dir_all(&flake_dir) {
-        return Err(KdError::new(
-            KdErrorKind::RuntimeError,
-            format!("Unable to create {}: {}", flake_dir.display(), error),
-        ));
-    }
+    std::fs::create_dir_all(&flake_dir)
+        .with_context(|| format!("Unable to create {}", flake_dir.display()))?;
 
     println!("Creating new environment");
     let mut cmd = Command::new("nix");
@@ -381,39 +359,22 @@ fn cmd_init(_: &State) -> Result<(), KdError> {
     }
 
     let user_config = PathBuf::from(flake_dir.clone()).join("uconfig.nix");
-    if let Err(error) = File::create(&user_config) {
-        return Err(KdError::new(
-            KdErrorKind::RuntimeError,
-            format!("Unable to create {}: {}", user_config.display(), error),
-        ));
-    }
+    File::create(&user_config)
+        .with_context(|| format!("Unable to create {}", user_config.display()))?;
 
     let direnv = curdir.join(".envrc");
     if direnv.exists() {
         let mut backup = direnv.clone();
         backup.set_extension("bup");
-        std::fs::copy(&direnv, backup).map_err(|e| {
-            KdError::new(
-                KdErrorKind::IOError(e),
-                "Failed to make backup of .envrc: {e}".to_owned(),
-            )
-        })?;
+        std::fs::copy(&direnv, backup).context("Failed to make backup of .envrc")?;
     }
 
     match &mut File::create(&direnv) {
         Ok(target) => {
-            writeln!(target, "use flake path:.kd/flake").map_err(|e| {
-                KdError::new(
-                    KdErrorKind::IOError(e),
-                    "Failed to overwrite .envrc: {e}".to_owned(),
-                )
-            })?;
+            writeln!(target, "use flake path:.kd/flake").context("Failed to overwrite .envrc")?;
         }
         Err(error) => {
-            return Err(KdError::new(
-                KdErrorKind::RuntimeError,
-                format!("Unable to create {}: {}", direnv.display(), error),
-            ));
+            bail!("Unable to create {}: {}", direnv.display(), error);
         }
     };
 
@@ -425,17 +386,16 @@ fn cmd_init(_: &State) -> Result<(), KdError> {
     Ok(())
 }
 
-fn cmd_build(state: &mut State) {
+fn cmd_build(state: &mut State) -> Result<()> {
     match generate_uconfig(state) {
         Ok(content) => {
             let mut file = std::fs::File::create(&state.user_config)
-                .expect("Failed to create user config uconfig.nix");
+                .context("Failed to create user config uconfig.nix")?;
             file.write_all(content.as_bytes())
-                .expect("Failed to write out uconfig.nix data");
+                .context("Failed to write out uconfig.nix data")?;
         }
         Err(error) => {
-            println!("Failed to generate nix config: {error}");
-            std::process::exit(1);
+            bail!("Failed to generate nix config: {error}");
         }
     }
 
@@ -449,22 +409,23 @@ fn cmd_build(state: &mut State) {
     }
 
     cmd.spawn()
-        .expect("Failed to spawn 'nix build'")
+        .context("Failed to spawn 'nix build'")?
         .wait()
-        .expect("'nix build' wasn't running");
+        .context("'nix build' wasn't running")?;
+
+    Ok(())
 }
 
-fn cmd_run(state: &mut State) {
+fn cmd_run(state: &mut State) -> Result<()> {
     match generate_uconfig(state) {
         Ok(content) => {
             let mut file = std::fs::File::create(&state.user_config)
-                .expect("Failed to create user config uconfig.nix");
+                .context("Failed to create user config uconfig.nix")?;
             file.write_all(content.as_bytes())
-                .expect("Failed to write out uconfig.nix data");
+                .context("Failed to write out uconfig.nix data")?;
         }
         Err(error) => {
-            println!("Failed to generate nix config: {error}");
-            std::process::exit(1);
+            bail!("Failed to generate nix config: {error}")
         }
     }
 
@@ -481,12 +442,14 @@ fn cmd_run(state: &mut State) {
     }
 
     cmd.spawn()
-        .expect("Failed to spawn 'nix run'")
+        .context("Failed to spawn 'nix run'")?
         .wait()
-        .expect("'nix run' wasn't running");
+        .context("'nix run' wasn't running")?;
+
+    Ok(())
 }
 
-fn cmd_update(state: &State) {
+fn cmd_update(state: &State) -> Result<()> {
     let package = format!("path:{}", state.flake_dir.display());
     let mut cmd = Command::new("nix");
     cmd.arg("flake")
@@ -500,22 +463,23 @@ fn cmd_update(state: &State) {
     }
 
     cmd.spawn()
-        .expect("Failed to spawn 'nix flake update'")
+        .context("Failed to spawn 'nix flake update'")?
         .wait()
-        .expect("'nix flake update' wasn't running");
+        .context("'nix flake update' wasn't running")?;
+
+    Ok(())
 }
 
-fn cmd_config(state: &mut State, output: Option<String>) {
+fn cmd_config(state: &mut State, output: Option<String>) -> Result<()> {
     match generate_uconfig(state) {
         Ok(content) => {
             let mut file = std::fs::File::create(&state.user_config)
-                .expect("Failed to create user config uconfig.nix");
+                .context("Failed to create user config uconfig.nix")?;
             file.write_all(content.as_bytes())
-                .expect("Failed to write out uconfig.nix data");
+                .context("Failed to write out uconfig.nix data")?;
         }
         Err(error) => {
-            println!("Failed to generate nix config: {error}");
-            std::process::exit(1);
+            bail!("Failed to generate nix config: {error}")
         }
     }
 
@@ -528,9 +492,9 @@ fn cmd_config(state: &mut State, output: Option<String>) {
     }
 
     cmd.spawn()
-        .expect("Failed to spawn 'nix build .#kconfig'")
+        .context("Failed to spawn 'nix build .#kconfig'")?
         .wait()
-        .expect("'nix build .#kconfig' wasn't running");
+        .context("'nix build .#kconfig' wasn't running")?;
 
     let output = if let Some(output) = output {
         PathBuf::from(output)
@@ -541,15 +505,16 @@ fn cmd_config(state: &mut State, output: Option<String>) {
     if output.exists() {
         let mut backup = output.clone();
         backup.set_extension("bup");
-        std::fs::copy(&output, backup).unwrap();
+        std::fs::copy(&output, backup).context("Failed to create config backup")?;
     }
 
     let source = state.envdir.clone().join("result");
-    std::fs::copy(source, &output).unwrap();
-    std::fs::set_permissions(output, std::fs::Permissions::from_mode(0o644)).unwrap();
+    std::fs::copy(source, &output).context("Failed to copy config to .config")?;
+    std::fs::set_permissions(output, std::fs::Permissions::from_mode(0o644))
+        .context("Failed to set 644 permission on config")
 }
 
-fn cmd_debug(state: &mut State, output: &bool) {
+fn cmd_debug(state: &mut State, output: &bool) -> Result<()> {
     match generate_uconfig(state) {
         Ok(content) => {
             if *output {
@@ -557,19 +522,26 @@ fn cmd_debug(state: &mut State, output: &bool) {
                     .stdin(Stdio::piped())
                     .arg("--quiet")
                     .spawn()
-                    .expect("alejandra (nix code formatter) failed to run");
-                write!(cmd.stdin.as_mut().unwrap(), "{}", content).unwrap();
-                cmd.wait().expect("'alejandra' failed to run");
+                    .context("alejandra (nix code formatter) failed to run")?;
+                write!(
+                    cmd.stdin
+                        .as_mut()
+                        .context("No input content for alejandra")?,
+                    "{}",
+                    content
+                )
+                .unwrap();
+                cmd.wait().context("'alejandra' failed to run")?;
             }
+            Ok(())
         }
         Err(error) => {
-            println!("Failed to generate nix config: {error}");
-            std::process::exit(1);
+            bail!("Failed to generate nix config: {error}")
         }
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     // All the command require .kd.toml. Only init can go without the config as it creates it
@@ -602,19 +574,14 @@ fn main() {
     }
 
     match &cli.command {
-        Some(Commands::Init {}) => {
-            if let Err(error) = cmd_init(&state) {
-                println!("{}", error);
-                std::process::exit(1);
-            }
-        }
+        Some(Commands::Init {}) => cmd_init(&state).context("Initialization failed"),
 
         Some(Commands::Build { name }) => {
             if let Some(name) = &name {
                 state.name = name.clone();
             }
 
-            cmd_build(&mut state);
+            cmd_build(&mut state)
         }
 
         Some(Commands::Run { name }) => {
@@ -622,19 +589,17 @@ fn main() {
                 state.name = name.clone();
             }
 
-            cmd_run(&mut state);
+            cmd_run(&mut state)
         }
 
-        Some(Commands::Update {}) => {
-            cmd_update(&state);
-        }
+        Some(Commands::Update {}) => cmd_update(&state),
 
         Some(Commands::Config { output, name }) => {
             if let Some(name) = &name {
                 state.name = name.clone();
             }
 
-            cmd_config(&mut state, output.clone());
+            cmd_config(&mut state, output.clone())
         }
 
         Some(Commands::Debug { config, name }) => {
@@ -642,9 +607,9 @@ fn main() {
                 state.name = name.clone();
             }
 
-            cmd_debug(&mut state, config);
+            cmd_debug(&mut state, config)
         }
 
-        None => {}
+        None => Ok(()),
     }
 }
